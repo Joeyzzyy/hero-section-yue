250058b530225af3f12857cf8ef849acd58c3c09
Yue Zhu <zhuyuejoey@gmail.com>
2024-11-30 14:42:48 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-const DOMAIN = process.env.DOMAIN;
-
-const isLocal = process.env.NODE_ENV === 'development';
-
 const nextConfig = {
-  images: {
-    domains: isLocal ? [] : [DOMAIN],
-    unoptimized: true,
-  },
   async redirects() {
     return [];
   },
